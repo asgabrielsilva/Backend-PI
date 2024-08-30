01488991f3f63812363310f93d40d0d50d9ec7b6
asgabrielsilva <gabrielalmeida290905@gmail.com>
2024-08-30 16:43:02 -0300

    Upload e associação de imagem

--- core/models/produto.py
+++ core/models/produto.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from uploader.models import Image
+
 class Produto(models.Model):
     class RaridadeProduto(models.IntegerChoices):
         QUATRO = 4, "4 Estrelas"
@@ -24,11 +26,35 @@ class Produto(models.Model):
         IMAGINARIO = 7, "Imaginário"
 
     nome = models.CharField(max_length=255)
-    descricao = models.CharField(max_length=32, null=True, blank=True)
+    descricao = models.CharField(max_length=1000, null=True, blank=True)
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True, verbose_name="Preço")
     caminho = models.IntegerField(choices=CaminhoProduto.choices, default=CaminhoProduto.DESTRUICAO)
     elemento = models.IntegerField(choices=ElementoProduto.choices, default=ElementoProduto.FISICO)
     tier = models.IntegerField(choices=RaridadeProduto.choices,  default=RaridadeProduto.QUATRO)
+    backgroundChar = models.ForeignKey(
+        Image,
+        related_name="+",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        default=None,
+    )
+    caminhoPng = models.ForeignKey(
+        Image,
+        related_name="+",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        default=None,
+    )
+    elementoPng = models.ForeignKey(
+        Image,
+        related_name="+",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        default=None,
+    )
 
     def __str__(self):
         return f"({self.id}) {self.nome} ({self.descricao})"
--- core/serializers/produto.py
+++ core/serializers/produto.py
@@ -1,8 +1,15 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SlugRelatedField
+
+from uploader.models import Image
+from uploader.serializers import ImageSerializer
 
 from core.models import Produto
 
 class ProdutoDetailSerializer(ModelSerializer): 
+    backgroundChar = ImageSerializer(required=False)
+    caminhoPng = ImageSerializer(required=False)
+    elementoPng = ImageSerializer(required=False)
+
     class Meta:
         model = Produto
         fields = "__all__"
@@ -14,6 +21,39 @@ class ProdutoListSerializer(ModelSerializer):
         fields = ("id", "nome", "preco")
 
 class ProdutoSerializer(ModelSerializer):
+    backgroundChar_attachment_key = SlugRelatedField(
+        source="backgroundChar",
+        queryset=Image.objects.all(),
+        slug_field="attachment_key",
+        required=False,
+        write_only=True,
+    )
+    backgroundChar = ImageSerializer(
+        required=False,
+        read_only=True
+    )
+    caminhoPng_attachment_key = SlugRelatedField(
+        source="caminhoPng",
+        queryset=Image.objects.all(),
+        slug_field="attachment_key",
+        required=False,
+        write_only=True,
+    )
+    caminhoPng = ImageSerializer(
+        required=False,
+        read_only=True
+    )
+    elementoPng_attachment_key = SlugRelatedField(
+        source="elementoPng",
+        queryset=Image.objects.all(),
+        slug_field="attachment_key",
+        required=False,
+        write_only=True,
+    )
+    elementoPng = ImageSerializer(
+        required=False,
+        read_only=True
+    )
     class Meta:
         model = Produto
         fields = "__all__"
