8861ae254b66cb74a197e75835a43748bcb6503a
asgabrielsilva <gabrielalmeida290905@gmail.com>
2024-09-09 11:11:41 -0300

    Inclusão do email do usuário e do status da compra na listagem da compra

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -1,13 +1,17 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import CharField, ModelSerializer
 
 from core.models import Compra
 
 class CompraSerializer(ModelSerializer):
+    usuario = CharField(source="user.email", read_only=True)
+    status = CharField(source="get_status_display", read_only=True)
     class Meta:
         model = Compra
         fields = "__all__"
 
 class CompraDetailSerializer(ModelSerializer):
+    usuario = CharField(source="user.email", read_only=True)
+    status = CharField(source="get_status_display", read_only=True)
     class Meta:
         model = Compra
         fields = "__all__"
